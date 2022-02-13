e0a950782b74b77bf784a00e4a4af0d59d512101
ttt <tschachtli.th@gmail.com>
2022-02-13 21:44:19 +0100

    first commit

---
+++ src/main/java/ch/ttt/demoawsrest/DemoAwsRestApplication.java
@@ -0,0 +1,13 @@
+package ch.ttt.demoawsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAwsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAwsRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/ttt/demoawsrest/DemoAwsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ttt.demoawsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAwsRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
